e20e01927b9229f8010448ca0e41da5c0636c7c3
Benjamin Polozune <bpolozun@uncc.edu>
2017-04-11 22:00:51 +0000

    Added main index, set rails routes, added CSS sheets for main and group.

--- Project/config/routes.rb
+++ Project/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'main/index'
+  
+  resources :group
+  
+  root 'main#index'
 end
