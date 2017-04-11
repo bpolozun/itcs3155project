e19c91290e20881b8db443efd157162872921ba4
Benjamin Polozune <bpolozun@uncc.edu>
2017-04-11 21:56:46 +0000

    Successfully added the Group model and migrated the db

--- Project/db/schema.rb
+++ Project/db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170411215519) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "groupName"
+    t.text     "description"
+    t.integer  "numberNeeded"
+    t.integer  "numberTotal"
+    t.string   "category"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end
