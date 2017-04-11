7067303e34ec4b6917a24ce06f604f85e40df1fe
Benjamin Polozune <bpolozun@uncc.edu>
2017-04-11 20:39:08 +0000

    Revert "Added main index, stylesheets, configured routes.tb"
    This reverts commit bbf8137ca261dad1c7232988dfcdae8453d04ec0.

--- Project/config/routes.rb
+++ Project/config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'main/index'
-  
-  resources :group
-  
-  root 'main#index'
-
 end
