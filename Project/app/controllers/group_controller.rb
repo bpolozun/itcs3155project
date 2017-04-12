77eec13f064d873a9e8850b9179dfb81cf6bac6f
Benjamin Polozune <bpolozun@uncc.edu>
2017-04-12 21:38:17 +0000

    Added group controller methods.

--- Project/app/controllers/group_controller.rb
+++ Project/app/controllers/group_controller.rb
@@ -1,2 +1,27 @@
 class GroupController < ApplicationController
+    
+    def new
+        
+    end
+    
+    def create
+        #render plain: params[:group].inspect
+        @group = group.new(params[:group])
+        
+        @group.save
+        redirect_to @group
+    end
+    
+    def index
+       @group = group.all 
+    end
+    
+    def edit
+       @group = group.find(params[:id]) 
+    end
+    
+    private
+    def group_parameters
+        params.require(:group).permit(:groupName, :desription, :numberNeeded, :numberTotal)
+    end
 end
