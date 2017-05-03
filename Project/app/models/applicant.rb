b59728a629f50718dd1bc7c5bc5dcb6705c3d4b4
Benjamin Polozune <bpolozun@uncc.edu>
2017-05-03 16:30:50 +0000

    Added input validation, destroy cascading.

--- Project/app/models/applicant.rb
+++ Project/app/models/applicant.rb
@@ -1,3 +1,5 @@
 class Applicant < ApplicationRecord
   belongs_to :group
+  validates :name, presence: true
+  validates :contact, presence: true
 end
--- Project/app/models/group.rb
+++ Project/app/models/group.rb
@@ -1,3 +1,5 @@
 class Group < ApplicationRecord
-    has_many :applicants
+    has_many :applicants, dependent: :destroy
+    validates :groupName, presence: true
+    validates :numberNeeded, presence: true, length: { minimum: 1}
 end
